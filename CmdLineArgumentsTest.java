1701713b911ed611d33b934bffea44715f944fc2
chandan1784 <32992458+chandan1784@users.noreply.github.com>
2020-08-05 19:11:31 +0530

    Add files via upload

---
+++ CmdLineArgumentsTest.java
@@ -0,0 +1,15 @@
+
+public class CmdLineArgumentsTest {
+	
+	
+	 public static void main(String args[]) {
+		 
+	
+	 System.out.println("welcome to JAVA training @ " +args[0]);
+	 System.out.println("welcome to Python training @ " +args[1]);
+	 System.out.println("welcome to CCNA training @ " +args[2]);
+	
+	 
+	 }
+
+}
---
+++ DollarToRupee.java
@@ -0,0 +1,16 @@
+
+public class DollarToRupee {
+	
+	
+	
+	 public static void main(String args[]) {
+		 
+		 
+		 int x=Integer.parseInt(args[0]);
+		 
+		 System.out.println(x*70);
+			
+	 }
+
+
+}
